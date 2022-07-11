a2358ced08c473d1d053033fc11e9fd8e669dce4
Austin <austin@foodtronix.com>
2022-07-11 09:54:07 -0500

    asd

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
-// create the express server here
 require('dotenv').config();
 
-const {PORT = 3000} =process.env
-
+const { PORT = 3000 } = process.env;
 const express = require('express');
 const server = express();
+
+const bodyParser = require('body-parser');
+server.use(bodyParser.json());
 
 const morgan = require('morgan');
 server.use(morgan('dev'));
 
-const cors = require('cors');
-server.use(cors());
+server.use((req, res, next) => {
+  console.log("<____Body Logger START____>");
+  console.log(req.body);
+  console.log("<_____Body Logger END_____>");
 
-const bodyParser = require('body-parser');
-server.use(bodyParser.json())
+  next();
+});
 
 const apiRouter = require('./api');
 server.use('/api', apiRouter);
 
-server.use((err, req, res, next)=>{
-    console.error(err.stack)
-    res.status(500).send("Something Failed!")
-})
+const { client } = require('./db');
+client.connect();
 
-const {client} = require('./db')
-
-server.listen(PORT, ()=>{
-    console.log('The server is listening on port', PORT)
-    client.connect();
-})
+server.listen(PORT, () => {
+  console.log("The server is up on port", PORT);
+});
